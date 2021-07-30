2a53b5b27fddae123bb02b6f4505c155c26f2c48
HaPhuong <phuongyuna520@gmail.com>
2021-07-30 09:30:22 +0700

    thiếu thư viện hay sao á

--- BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ BroadcastReceiver/app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    BroadcastReceiver broadcastReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        broadcastReceiver = new InternetCheckService();
+        checkInternet();
+
+    }
+
+    private void checkInternet() {
+        registerReceiver(broadcastReceiver,
+                new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(broadcastReceiver);
     }
 }
