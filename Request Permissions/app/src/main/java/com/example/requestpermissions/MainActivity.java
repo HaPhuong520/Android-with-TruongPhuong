a1791b562f51f0420df2f0ae88125ed63866f8e1
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-07-30 15:46:54 +0700

    import library

--- Request Permissions/app/src/main/java/com/example/requestpermissions/MainActivity.java
+++ Request Permissions/app/src/main/java/com/example/requestpermissions/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.gun0912.tedpermission.PermissionListener;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
