75b5204f91667e90f51a517e38ff6a98ae2db673
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-07-30 15:54:33 +0700

    gần xong r

--- Request Permissions/app/src/main/java/com/example/requestpermissions/MainActivity.java
+++ Request Permissions/app/src/main/java/com/example/requestpermissions/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.requestpermissions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     private void requestPermissions() {
-        PermissionListener permissionListener=new PermissionListener
+        PermissionListener permissionListener=new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this,"permission grandted",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(List<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this,"permission Denied"+deniedPermissions.toString(),Toast.LENGTH_SHORT).show();
+            }
+        };
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("kdsknvn")
+                .setPermissions(Manifest.permission.CAMERA,Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
     }
 }
