199257b1d862bc234dab433717d24402ca1b0255
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-08-02 08:00:02 +0700

    sdk bug

--- GenerateQRCode/app/src/main/java/com/example/generateqrcode/MainActivity.java
+++ GenerateQRCode/app/src/main/java/com/example/generateqrcode/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.generateqrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.MultiAutoCompleteTextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edInput;
+    Button btGenerate;
+    ImageView ivOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edInput=findViewById(R.id.ed_input);
+        btGenerate=findViewById(R.id.bt_generate);
+        ivOutput=findViewById(R.id.iv_output);
+        btGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sText=edInput.getText().toString().trim();
+                MultiFormatWriter writer=new MultiFormatWriter();
+                try {
+                    BitMatrix matrix=writer.encode(sText, BarcodeFormat.QR_CODE,350,350);
+                    BarcodeEncoder encoder =new BarcodeEncoder();
+                    Bitmap bitmap=encoder.createBitmap(matrix);
+                    ivOutput.setImageBitmap(bitmap);
+                    InputMethodManager manager=(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    manager.hideSoftInputFromWindow(edInput.getApplicationWindowToken(),0);
+                } catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
