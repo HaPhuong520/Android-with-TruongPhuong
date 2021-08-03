9d725497f342a6e35c0fc5b055df7128940424ac
HaPhuong <phuongyuna520@gmail.com>
2021-08-03 09:31:10 +0700

    CCP update

--- CountryCodePickerLibrary/app/src/main/java/com/example/contrycodepicker/MainActivity.java
+++ CountryCodePickerLibrary/app/src/main/java/com/example/contrycodepicker/MainActivity.java
@@ -3,18 +3,66 @@ package com.example.contrycodepicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
+    private  CountryCodePicker countryCodePicker;
+    private EditText edtPhoneNumber;
+    private ImageView imgCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        AnhXa();
 
-        CountryCodePicker countryCodePicker = findViewById(R.id.ccp);
-        countryCodePicker.showFullName(true);
+        countryCodePicker.registerCarrierNumberEditText(edtPhoneNumber);
+
+
+
+        countryCodePicker.setPhoneNumberValidityChangeListener(new CountryCodePicker.PhoneNumberValidityChangeListener() {
+            @Override
+            public void onValidityChanged(boolean isValidNumber) {
+               if (isValidNumber){
+                   imgCheck.setImageDrawable(R.drawable.dung);
+               } else {
+                   imgCheck.setImageDrawable(R.drawable.sai);
+               }
+            }
+        });
+
+        edtPhoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String input = s.toString().trim();
+                if (input.length() > 0){
+                    imgCheck.setVisibility(View.VISIBLE);
+                } else {
+                    imgCheck.setVisibility(View.GONE);
+                }
+
+            }
+        });
+    }
+
+    private void AnhXa() {
+        countryCodePicker = findViewById(R.id.ccp);
+        edtPhoneNumber = findViewById(R.id.edt_phone_number);
+        imgCheck = findViewById(R.id.img_check);
     }
 }
