535ca8e25802608a97e6ced94c0d1363b5e35eff
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-08-03 16:33:59 +0700

    xong r nhưng chưa có link mp3

--- PlayMusicOnlineFromURL/app/src/main/java/com/example/playmusiconlinefromurl/MainActivity.java
+++ PlayMusicOnlineFromURL/app/src/main/java/com/example/playmusiconlinefromurl/MainActivity.java
@@ -2,13 +2,105 @@ package com.example.playmusiconlinefromurl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imagePlayPause;
+    TextView textCurrentTime,textTotalDuration;
+    SeekBar playerSeekBar;
+    MediaPlayer mediaPlayer;
+    Handler handler=new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagePlayPause=findViewById(R.id.imagePlayPause);
+        textCurrentTime=findViewById(R.id.textCurrentTime);
+        textTotalDuration=findViewById(R.id.textTotalDuration);
+        playerSeekBar=findViewById(R.id.playerSeekBar);
+        mediaPlayer=new MediaPlayer();
+        playerSeekBar.setMax(100);
+
+        imagePlayPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    handler.removeCallbacks(updater);
+                    mediaPlayer.pause();
+                    imagePlayPause.setImageResource(R.drawable.ic_baseline_play_circle_filled_24);
+                }
+                else
+                {
+                    mediaPlayer.start();
+                    imagePlayPause.setImageResource(R.drawable.ic_baseline_pause_circle_filled_24);
+                    updateSeekBar();
+                }
+            }
+        });
+        prepareMediaPlayer();
+        playerSeekBar.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                SeekBar seekBar= (SeekBar) v;
+                int playPosition=(mediaPlayer.getDuration()/100)*seekBar.getProgress();
+                mediaPlayer.seekTo(playPosition);
+                textCurrentTime.setText(milliSecondsToTimer(mediaPlayer.getCurrentPosition()));
+                return false;
+            }
+        });
+    }
+    private void prepareMediaPlayer(){
+        try {
+            mediaPlayer.setDataSource("https://drive.google.com/file/d/1MBQQIDOBtB6uJ1KjqsX1LiFX7MdhJnaY/view");
+            mediaPlayer.prepare();
+            textTotalDuration.setText(milliSecondsToTimer(mediaPlayer.getDuration()));
+        }catch (Exception e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+    private Runnable updater=new Runnable() {
+        @Override
+        public void run() {
+            updateSeekBar();
+            long currentDuration=mediaPlayer.getCurrentPosition();
+            textCurrentTime.setText(milliSecondsToTimer(currentDuration));
+        }
+    };
+    private  void updateSeekBar(){
+        if(mediaPlayer.isPlaying()){
+            playerSeekBar.setProgress((mediaPlayer.getCurrentPosition()/mediaPlayer.getDuration())*100);
+            handler.postDelayed(updater,1000);
+        }
+    }
+    private String  milliSecondsToTimer(long milliSeconds){
+        String timerString="";
+        String secondsString;
+        int hours= (int) (milliSeconds/(1000*60*60));
+        int minutes= (int) ((milliSeconds%(1000*60*60))/(1000*60));
+        int seconds= (int) ((milliSeconds%(1000*60*60))%(1000*60)/1000);
+        if(hours>0){
+            timerString=hours+":";
+
+        }
+        if(seconds<10){
+            secondsString="0"+seconds;
+        }
+        else
+        {
+            secondsString=""+seconds;
+
+        }
+        timerString=timerString+minutes+":"+secondsString;
+        return timerString;
     }
 }
