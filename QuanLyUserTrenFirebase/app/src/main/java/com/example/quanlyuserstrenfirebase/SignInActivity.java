a56b4ecf6ae8aae005c6d8723476be841ffc33bf
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-08-05 09:07:07 +0700

    thêm(chức năng):đăng nhập,đăng kí,màn hình chờ

---
+++ QuanLyUserTrenFirebase/app/src/main/java/com/example/quanlyuserstrenfirebase/SignInActivity.java
@@ -0,0 +1,33 @@
+package com.example.quanlyuserstrenfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+public class SignInActivity extends AppCompatActivity {
+    private LinearLayout layoutSignUp;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.id.activity_sign_in);
+        initUi();
+        initListener();
+    }
+    private void initUi(){
+        layoutSignUp=findViewById(R.id.layout_sign_up);
+    }
+    private void initListener(){
+        layoutSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignInActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
