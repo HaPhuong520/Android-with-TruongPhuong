1b35803c736af1fc37fd069e3a04875e7cba2cd7
Truong Thi Thu Phuong <phuongtruong472001@gmail.com>
2021-08-06 08:34:08 +0700

    thêm(chức năng):cập nhật profile

--- QuanLyUserTrenFirebase/app/src/main/java/com/example/quanlyuserstrenfirebase/MainActivity.java
+++ QuanLyUserTrenFirebase/app/src/main/java/com/example/quanlyuserstrenfirebase/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.quanlyuserstrenfirebase;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,33 +14,64 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.bumptech.glide.Glide;
 import com.example.quanlyuserstrenfirebase.fragment.FavoriteFragment;
 import com.example.quanlyuserstrenfirebase.fragment.HistoryFragment;
 import com.example.quanlyuserstrenfirebase.fragment.HomeFragment;
+import com.example.quanlyuserstrenfirebase.fragment.MyProfileFragment;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final int FRAGMENT_HOME=0;
     private static final int FRAGMENT_FAVORITE=1;
     private static final int FRAGMENT_HISTORY=2;
+    private static final int FRAGMENT_MY_FROFILE=3;
     private int mCurrentFragment=FRAGMENT_HOME;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
 
     private ImageView imgAvt;
     private TextView tvName,tvEmail;
+    public static final int MY_REQUEST_CODE=10;
+
+    private final MyProfileFragment mMyProfileFragment=new MyProfileFragment();
+    final private ActivityResultLauncher<Intent> mActivityResultLauncher=registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if(result.getResultCode()==RESULT_OK){
+                Intent intent=result.getData();
+                if (intent==null){
+                    return;
+                }
+                Uri uri=intent.getData();
+                mMyProfileFragment.setmUri(uri);
+                try {
+                    Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                    mMyProfileFragment.setBitmapImageview(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -89,6 +124,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             startActivity(intent);
             finish();
         }
+        else if(id==R.id.nav_my_profile) {
+            if(mCurrentFragment!=FRAGMENT_MY_FROFILE){
+                replaceFragment(mMyProfileFragment);
+                mCurrentFragment=FRAGMENT_MY_FROFILE;
+            }
+        }
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
@@ -106,7 +147,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         transaction.replace(R.id.content_frame,fragment);
         transaction.commit();
     }
-    private void showUserInformation(){
+    public void showUserInformation(){
         FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
         if(user==null){
             return ;
@@ -128,4 +169,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==MY_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }
+            else
+            {
+                Toast.makeText(MainActivity.this,"vui long cho phep truy cap may anh",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void openGallery(){
+        Intent intent=new Intent();
+        intent.setType("image/*");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        mActivityResultLauncher.launch(Intent.createChooser(intent,"Select picture"));
+    }
 }
---
+++ QuanLyUserTrenFirebase/app/src/main/java/com/example/quanlyuserstrenfirebase/fragment/MyProfileFragment.java
@@ -0,0 +1,137 @@
+package com.example.quanlyuserstrenfirebase.fragment;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.example.quanlyuserstrenfirebase.MainActivity;
+import com.example.quanlyuserstrenfirebase.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+import static com.example.quanlyuserstrenfirebase.MainActivity.MY_REQUEST_CODE;
+
+
+public class MyProfileFragment extends Fragment {
+    private View mView;
+    private ImageView imgAvatar;
+    private EditText edtFullName,edtEmail;
+    private Button btnUpdateProfile;
+    Uri mUri;
+    private  MainActivity mMainActivity;
+    private ProgressDialog dialog;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       dialog=new ProgressDialog(getActivity());
+        mView= inflater.inflate(R.layout.fragment_my_profile, container, false);
+        initUi();
+        mMainActivity= (MainActivity) getActivity();
+        setUserInformation();
+        initListener();
+        return mView;
+    }
+
+    private void initUi(){
+        imgAvatar=mView.findViewById(R.id.img_avatar);
+        edtFullName=mView.findViewById(R.id.edt_full_name);
+        edtEmail=mView.findViewById(R.id.edt_email);
+        btnUpdateProfile=mView.findViewById(R.id.btn_update_profile);
+    }
+    private void setUserInformation() {
+
+        FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+        if(user==null){
+            return;
+        }
+        else
+        {
+            edtFullName.setText(user.getDisplayName());
+            edtEmail.setText(user.getEmail());
+            Glide.with(getActivity()).load(user.getPhotoUrl()).error(R.drawable.ic_my_profile).into(imgAvatar);
+
+        }
+    }
+    private void initListener() {
+        imgAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickRequestPermission();
+            }
+        });
+        btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickUpdatePrifile();
+            }
+        });
+    }
+
+    private void onClickRequestPermission() {
+
+        if(mMainActivity==null){
+            return;
+        }
+        if(Build.VERSION.SDK_INT<Build.VERSION_CODES.M){
+            mMainActivity.openGallery();
+            return;
+        }
+        if(getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
+            mMainActivity.openGallery();
+        }else{
+            String [] permission={Manifest.permission.READ_EXTERNAL_STORAGE};
+            getActivity().requestPermissions(permission,MY_REQUEST_CODE);
+        }
+    }
+
+    public void setmUri(Uri mUri) {
+        this.mUri = mUri;
+    }
+
+    public void setBitmapImageview(Bitmap bitmap){
+        imgAvatar.setImageBitmap(bitmap);
+    }
+    private void onClickUpdatePrifile() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user==null){
+            return;
+        }
+        dialog.show();
+        String fullName=edtFullName.getText().toString().trim();
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(fullName)
+                .setPhotoUri(mUri)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            dialog.dismiss();
+                            Toast.makeText(getActivity(),"Update Profile Success",Toast.LENGTH_SHORT).show();
+                            mMainActivity.showUserInformation();
+                        }
+                    }
+                });
+    }
+}
